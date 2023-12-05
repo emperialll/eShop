820a204d08e87e3d7621d11dded424a1aa94edbc
Shahriar <sh.fa84@gmail.com>
2023-12-05 06:43:59 +0100

    Builds checkout component main sections

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,15 +1,12 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./Navbar";
-import HeroCarousel from "./HeroCarousel";
-import ProductGrid from "./ProductGrid";
-import CategoryList from "./CategoryList";
 
 const Checkout = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main" "footer"`,
-        lg: `"nav nav" "hero hero" "aside main" " footer footer"`,
+        base: `"nav" "main" "checkout" "footer"`,
+        lg: `"nav nav" "banner banner" "main checkout" " footer footer"`,
       }}
       templateColumns={{
         base: "1fr",
@@ -23,18 +20,14 @@ const Checkout = () => {
 
       {/* ----******---- HERO SECTION ----******---- */}
       <Show above="lg">
-        <GridItem area="hero"></GridItem>
-      </Show>
-
-      {/* ----******---- SIDE MENU ----******---- */}
-      <Show above="lg">
-        <GridItem area="aside">
-          <CategoryList />
-        </GridItem>
+        <GridItem area="banner">Banner</GridItem>
       </Show>
 
       {/* ----******---- MAIN ----******---- */}
-      <GridItem area="main"></GridItem>
+      <GridItem area="main">Order List</GridItem>
+
+      {/* ----******---- Chcekout Section ----******---- */}
+      <GridItem area="checkout">Checkout Summary</GridItem>
 
       {/* ----******---- FOOTER ----******---- */}
       <GridItem area="footer">Footer</GridItem>
